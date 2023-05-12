c899c7c6637498f10839cd2692e13bb0049846ad
Richard Schmidt <richardnew12511@gmail.com>
2023-05-11 20:25:07 -0500

    add styling

---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+import type { PropsWithChildren } from "react";
+
+export function PageLayout(props: PropsWithChildren) {
+  return (
+    <main className="flex h-screen justify-center bg-teal-400">
+      <div className="flex h-full w-full max-w-2xl flex-col border-x border-slate-400 bg-orange-500">
+        {props.children}
+      </div>
+    </main>
+  );
+}
